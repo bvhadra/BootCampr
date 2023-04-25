23fae368eabc8febf5c81a78f45277ade78087b2
Guy Mulligan <guymulligan@outlook.com>
2023-04-25 16:05:01 +0100

    dummyComments made

---
+++ bootcampr/src/dummyComments.js
@@ -0,0 +1,8 @@
+const dummyComments = [
+    {name: 'Steve',  comment: 'Love this!'}, 
+    {name: 'Brenda', comment: 'Have you tried turning it off and on again...'},
+    {name: 'Ricardo',  comment: 'Check out this link: google.com'},
+    {name: 'Terrence', comment: 'Anyone seen my sock?'}
+];
+
+export default dummyComments;
