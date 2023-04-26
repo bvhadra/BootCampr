b5760f593872d7cccf349eafee57820b4ebc940b
lozzolloz <124617157+lozzolloz@users.noreply.github.com>
2023-04-26 16:35:02 +0100

    updated css

--- bootcampr/src/components/NewPost/NewPost.js
+++ bootcampr/src/components/NewPost/NewPost.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./NewPost.css";
-import CommentList from '../CommentList/CommentList'
+import CommentList from "../CommentList/CommentList";
 
 // props needed from app to show name, category and post from InputForm.
 function NewPost({ name, category, post }) {
-  const categoryClassName = `${category}`
-  
+  const categoryClassName = `${category}`;
 
   return (
-    <div className={categoryClassName}>
-      <h3>{name}</h3>
-      <h6>{category}</h6>
-      <p>{post}</p>
-      <CommentList />
+    <div>
+      <div className={categoryClassName}>
+        <h3>{name}</h3>
+        <h6>{category}</h6>
+        <p>{post}</p>
+      </div>
+      <div className={categoryClassName}>
+        <CommentList />
+      </div>
     </div>
   );
 }
