e9a3a019b19e28320895f375e57cdd5831345050
CGreen789 <112316339+CGreen789@users.noreply.github.com>
2023-04-27 16:44:58 +0100

    different dummy data added to the bootcampr app

--- bootcampr/src/components/data/dummyPosts.js
+++ bootcampr/src/components/data/dummyPosts.js
@@ -1,16 +1,63 @@
 const dummyPosts = [
-  { name: "Steve", category: "general", post: "Great weather today" },
   {
-    name: "Brenda",
+    name: "John",
+    category: "general",
+    post: "Today's lecture on useState was really informative. I learnt a lot!",
+  },
+  {
+    name: "Sarah",
+    category: "random",
+    post: "Does anyone want to grab lunch during the lunch break today? I'm in Nottingham",
+  },
+  {
+    name: "Mike",
     category: "funny",
-    post: "Why should you never date a tennis player? Because love means nothing to them",
+    post: "Why did the developer quit his job? He didn't get arrays.",
   },
   {
-    name: "Ricardo",
+    name: "Emily",
     category: "question",
-    post: "Mayo or ketchup? Can't decide which is best for my milkshake",
+    post: "Has anyone used React before? I'm struggling with it and could use some advice.",
   },
-  { name: "Terrence", category: "random", post: "I've lost a sock..." },
+  {
+    name: "Alex",
+    category: "general",
+    post: "Just finished our group presentation! Feeling accomplished.",
+  },
+  {
+    name: "Kim",
+    category: "random",
+    post: "What are some good study spots in Birmingham?",
+  },
+  {
+    name: "Tom",
+    category: "funny",
+    post: "I told my computer to go to sleep, but it stayed up all night coding.",
+  },
+  {
+    name: "Jen",
+    category: "question",
+    post: "Any tips for Jest tests? I'm running into lots of errors.",
+  },
+  {
+    name: "David",
+    category: "general",
+    post: "I'm really enjoying the community here. Everyone is so helpful. Thanks everyone.",
+  },
+  {
+    name: "Lisa",
+    category: "random",
+    post: "Just found out there's a new coffee shop opening near me in Manchester. Anyone want to check it out?",
+  },
+  {
+    name: "Mark",
+    category: "funny",
+    post: "That happy birthday singing today was certainly something special!",
+  },
+  {
+    name: "Sophie",
+    category: "question",
+    post: "What's the best way to approach a coding challenge during an interview?",
+  },
 ];
-
 export default dummyPosts;
